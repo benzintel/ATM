c1af2cfd698e52ae6b6d948244abd43d0ee8dda6
Sarut Yentakham <sarut@getlinks.co>
2018-09-08 00:25:13 +0700

    update shell

--- start.sh
+++ start.sh
@@ -1,15 +1,1 @@
-echo "install or start:"
-read START 
-
-if [ "$START" == "install" ]; then
-	# install laravel
-	echo "------------------Installing Vender Laravel------------------"
-	cd laravel
-	composer install
-
-	sleep 5
-fi
-
-echo "------------------Start Docker------------------"
-#start docker start
 docker-compose up
